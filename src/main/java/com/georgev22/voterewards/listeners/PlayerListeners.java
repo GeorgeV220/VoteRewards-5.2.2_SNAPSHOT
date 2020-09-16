71fffd1d83b5a74e490303441473b44b96948270
GeorgeV22 <georgev22.com@gmail.com>
2020-09-16 18:55:01 +0300

    Replace SoundUtil with XSound

--- src/main/java/com/georgev22/voterewards/listeners/PlayerListeners.java
+++ src/main/java/com/georgev22/voterewards/listeners/PlayerListeners.java
@@ -1,9 +1,13 @@
 package com.georgev22.voterewards.listeners;
 
-import java.util.EnumSet;
-import java.util.Set;
-
+import com.cryptomorin.xseries.XSound;
+import com.georgev22.voterewards.VoteRewardPlugin;
+import com.georgev22.voterewards.playerdata.PartyOptions;
+import com.georgev22.voterewards.playerdata.UserVoteData;
+import com.georgev22.voterewards.playerdata.VotePartyUtils;
+import com.georgev22.voterewards.utilities.MaterialUtil;
 import com.georgev22.voterewards.utilities.Updater;
+import com.georgev22.voterewards.utilities.Utils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,95 +18,90 @@ import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import com.georgev22.voterewards.VoteRewardPlugin;
-import com.georgev22.voterewards.playerdata.PartyOptions;
-import com.georgev22.voterewards.playerdata.UserVoteData;
-import com.georgev22.voterewards.playerdata.VotePartyUtils;
-import com.georgev22.voterewards.utilities.MaterialUtil;
-import com.georgev22.voterewards.utilities.SoundUtil;
-import com.georgev22.voterewards.utilities.Utils;
+import java.util.EnumSet;
+import java.util.Set;
 
 public class PlayerListeners implements Listener {
 
-	private final VoteRewardPlugin plugin;
+    private final VoteRewardPlugin plugin;
 
-	public PlayerListeners(final VoteRewardPlugin plugin) {
-		this.plugin = plugin;
-	}
+    public PlayerListeners(final VoteRewardPlugin plugin) {
+        this.plugin = plugin;
+    }
 
-	@EventHandler
-	public void onJoin(PlayerLoginEvent event) {
-		UserVoteData.getUser(event.getPlayer().getUniqueId()).setupUser();
-		if (plugin.getConfig().getBoolean("Options.updater")) {
-			if (event.getPlayer().hasPermission("voterewards.updater") || event.getPlayer().isOp()) {
-				new Updater(event.getPlayer());
-			}
-		}
-	}
+    @EventHandler
+    public void onJoin(PlayerLoginEvent event) {
+        UserVoteData.getUser(event.getPlayer().getUniqueId()).setupUser();
+        if (plugin.getConfig().getBoolean("Options.updater")) {
+            if (event.getPlayer().hasPermission("voterewards.updater") || event.getPlayer().isOp()) {
+                new Updater(event.getPlayer());
+            }
+        }
+    }
 
-	@EventHandler
-	public void onSignChange(SignChangeEvent e) {
-		if (e.getLine(0).equalsIgnoreCase("[VoteRewards]")) {
-			if (e.getLine(1).isEmpty()) {
-			}
+    @EventHandler
+    public void onSignChange(SignChangeEvent e) {
+        if (e.getLine(0).equalsIgnoreCase("[VoteRewards]")) {
+            if (e.getLine(1).isEmpty()) {
+            }
 
-		}
-	}
+        }
+    }
 
-	private final Set<Action> clicks = EnumSet.of(Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK,
-			Action.LEFT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK);
+    private final Set<Action> clicks = EnumSet.of(Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK,
+            Action.LEFT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK);
 
-	@SuppressWarnings("deprecation")
-	@EventHandler
-	public void onPlayerInteract(PlayerInteractEvent event) {
+    @SuppressWarnings("deprecation")
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
 
-		if (!clicks.contains(event.getAction())) {
-			return;
-		}
+        if (!clicks.contains(event.getAction())) {
+            return;
+        }
 
-		final Player p = event.getPlayer();
+        final Player p = event.getPlayer();
 
-		final ItemStack item = p.getInventory().getItemInHand();
+        final ItemStack item = p.getInventory().getItemInHand();
 
-		if (item.getType() != MaterialUtil.valueOf(plugin.getConfig().getString("VoteParty.crate.item"))
-				.parseMaterial()) {
-			return;
-		}
+        if (item.getType() != MaterialUtil.valueOf(plugin.getConfig().getString("VoteParty.crate.item"))
+                .parseMaterial()) {
+            return;
+        }
 
-		final ItemMeta meta = item.getItemMeta();
+        final ItemMeta meta = item.getItemMeta();
 
-		if (!(meta != null && meta.hasDisplayName())) {
-			return;
-		}
+        if (!(meta != null && meta.hasDisplayName())) {
+            return;
+        }
 
-		final String itemName = plugin.getConfig().getString("VoteParty.crate.name");
-		if (itemName == null) {
-			return;
-		}
+        final String itemName = plugin.getConfig().getString("VoteParty.crate.name");
+        if (itemName == null) {
+            return;
+        }
 
-		if (!meta.getDisplayName().equals(Utils.colorize(itemName))) {
-			return;
-		}
+        if (!meta.getDisplayName().equals(Utils.colorize(itemName))) {
+            return;
+        }
 
-		final int amount = item.getAmount();
+        final int amount = item.getAmount();
 
-		if (amount == 1) {
-			p.getInventory().clear(p.getInventory().getHeldItemSlot());
-		} else {
-			item.setAmount(amount - 1);
-		}
+        if (amount == 1) {
+            p.getInventory().clear(p.getInventory().getHeldItemSlot());
+        } else {
+            item.setAmount(amount - 1);
+        }
 
-		VotePartyUtils.getInstance().chooseRandom(PartyOptions.RANDOM.isEnabled(), p);
+        VotePartyUtils.getInstance().chooseRandom(PartyOptions.RANDOM.isEnabled(), p);
 
-		if (PartyOptions.SOUND_CRATE.isEnabled()) {
+        if (PartyOptions.SOUND_CRATE.isEnabled()) {
 
-			// SoundUtil.valueOf(plugin.getConfig().getString("Sounds.Crate")).getSound()
+            // SoundUtil.valueOf(plugin.getConfig().getString("Sounds.Crate")).getSound()
 
-			SoundUtil.getSound(plugin.getConfig().getString("Sounds.Crate"))
-					.ifPresent(sound -> p.playSound(p.getLocation(), sound, 1000, 1));
+            XSound.matchXSound(plugin.getConfig().getString("Sounds.Crate"))
+                    .ifPresent(sound -> p.playSound(p.getLocation(), sound.parseSound(), 1000, 1));
 
-		}
-		event.setCancelled(true);
+        }
+        event.setCancelled(true);
 
-	}
+    }
 }
