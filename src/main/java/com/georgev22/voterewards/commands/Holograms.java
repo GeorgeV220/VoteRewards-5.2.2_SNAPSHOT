e1a30dad15714711abc925131fd1eba479435f32
GeorgeV220 <georgev22.github@gmail.com>
2021-01-27 22:23:25 +0200

    Latest Changes Removed modules Added type check and messages in Holograms command Code comments fixes (Not visible in commit changes) Some other changes

--- src/main/java/com/georgev22/voterewards/commands/Holograms.java
+++ src/main/java/com/georgev22/voterewards/commands/Holograms.java
@@ -37,11 +37,16 @@ public class Holograms extends BukkitCommand {
     @Override
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
         if (!testPermission(sender)) return true;
-        if (!Bukkit.getServer().getPluginManager().isPluginEnabled("HolographicDisplays")) return true;
+        if (!Bukkit.getServer().getPluginManager().isPluginEnabled("HolographicDisplays")) {
+            Utils.msg(sender, "&c&l(!) &cHolographicDisplays is not enabled!");
+            return true;
+        }
+
         if (args.length == 0) {
             Utils.msg(sender, "&c&l(!) &cNot enough arguments!");
             return true;
         }
+
         Player player = (Player) sender;
         if (args[0].equalsIgnoreCase("create")) {
             if (args.length < 3) {
@@ -54,7 +59,7 @@ public class Holograms extends BukkitCommand {
                 return true;
             }
 
-            if (m.getConfig().get("Holograms." + args[2]) != null) {
+            if (m.getConfig().get("Holograms." + args[2]) == null) {
                 Utils.msg(sender, "&c&l(!) &cHologram type doesn't exists!");
                 return true;
             }
@@ -68,6 +73,8 @@ public class Holograms extends BukkitCommand {
 
             HolographicDisplays.getPlaceholderMap().clear();
 
+            Utils.msg(sender, "&a&l(!) &aHologram " + args[1] + " with type " + args[2] + " successfully created!");
+
         } else if (args[0].equalsIgnoreCase("remove")) {
             if (args.length == 1) {
                 Utils.msg(player, "&c&l(!) &cUsage: /hologram remove <hologramName>");
@@ -80,15 +87,17 @@ public class Holograms extends BukkitCommand {
             }
 
             HolographicDisplays.remove(args[1]);
+
+            Utils.msg(sender, "&a&l(!) &aHologram " + args[1] + " successfully removed!");
         } else if (args[0].equalsIgnoreCase("update")) {
             if (HolographicDisplays.hologramExists(args[1])) {
                 Hologram hologram = HolographicDisplays.getHologramMap().get(args[1]);
-                //String[] lines = {"%top1%", "%top2%", "%top3%", "%top4%", "%top5%"};
                 HolographicDisplays.updateHologram(hologram, m.getConfig().getStringList("Holograms." + args[2]).toArray(new String[0]), HolographicDisplays.getPlaceholderMap());
                 HolographicDisplays.getPlaceholderMap().clear();
                 HolographicDisplays.hide(hologram, player);
                 Bukkit.getScheduler().runTaskLaterAsynchronously(m, () -> HolographicDisplays.show(hologram, player), 20);
                 Utils.msg(player, args[1] + " " + args[2]);
+                Utils.msg(sender, "&a&l(!) &aHologram " + args[1] + " successfully updated!");
             } else {
                 Utils.msg(player, "&c&l(!) &cHologram doesn't exists!");
             }
