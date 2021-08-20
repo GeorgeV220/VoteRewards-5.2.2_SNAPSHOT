e4fd645db9e97cb53a2afb8b04ac01ad5b3751c2
George V <georgev22.github@gmail.com>
2021-08-20 23:39:51 +0300

    Some tests, ignore this commit

--- src/main/java/com/georgev22/voterewards/utilities/inventory/inventories/VoteInventory.java
+++ src/main/java/com/georgev22/voterewards/utilities/inventory/inventories/VoteInventory.java
@@ -1,5 +1,6 @@
 package com.georgev22.voterewards.utilities.inventory.inventories;
 
+import com.georgev22.voterewards.VoteRewardPlugin;
 import com.georgev22.voterewards.utilities.OptionsUtil;
 import com.georgev22.voterewards.utilities.inventory.ItemBuilder;
 import com.georgev22.voterewards.utilities.player.UserVoteData;
@@ -9,6 +10,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
+import java.security.SecureRandom;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.Map;
@@ -16,12 +18,36 @@ import java.util.Map;
 @Beta
 public class VoteInventory {
 
+
+    private static final SecureRandom random = new SecureRandom();
+
+    public static <T extends Enum<?>> T randomEnum(Class<T> clazz) {
+        int x = random.nextInt(clazz.getEnumConstants().length);
+        return clazz.getEnumConstants()[x];
+    }
+
+
     public void openInventory(Player player) {
         UserVoteData userVoteData = UserVoteData.getUser(player);
 
         Inventory inventory = Bukkit.createInventory(null, 54, "Vote Inventory");
 
-        for (Map.Entry<String, Long> b : userVoteData.getServicesLastVote().entrySet()) {
+        for (String serviceName : VoteRewardPlugin.getInstance().getConfig().getConfigurationSection("Options.daily.services").getKeys(false)) {
+            ItemBuilder itemBuilder = new ItemBuilder(randomEnum(Material.class));
+            itemBuilder.title(serviceName);
+            boolean exists = (userVoteData.getServicesLastVote().get(serviceName) != null);
+            if (exists) {
+                if (userVoteData.getServicesLastVote().getLong(serviceName) + (OptionsUtil.DAILY_HOURS.getIntValue() * 60 * 60 * 1000) > System.currentTimeMillis()) {
+                    itemBuilder.lore("You can't vote!");
+                } else {
+                    itemBuilder.lore("You can vote!");
+                }
+            } else {
+                itemBuilder.lore("You can vote!");
+            }
+            inventory.addItem(itemBuilder.build());
+        }
+        /*for (Map.Entry<String, Long> b : userVoteData.getServicesLastVote().entrySet()) {
             String serviceName = b.getKey();
             Long serviceLastVote = b.getValue();
 
@@ -32,7 +58,7 @@ public class VoteInventory {
                     serviceLastVote + (OptionsUtil.DAILY_HOURS.getIntValue() * 60 * 60 * 1000) > System.currentTimeMillis() ? "You cannot vote yet" : "You can vote again");
 
             inventory.addItem(itemBuilder.build());
-        }
+        }*/
 
         player.openInventory(inventory);
     }
